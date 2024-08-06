8b45ef5092c52c14bf620b9937e2805853c2d4ab
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-06 12:46:02 +0100

    to email changed

--- src/api/inqury/content-types/inqury/lifecycles.js
+++ src/api/inqury/content-types/inqury/lifecycles.js
@@ -7,7 +7,7 @@ module.exports = {
           .plugin("email")
           .service("email")
           .send({
-            to: "noreply@theglobalproperty.co.uk",
+            to: "property@theglobalproperty.co.uk",
             from: "noreply@theglobalproperty.co.uk",
             subject: "New Inquiry From ",
             text: `You have new inqury from healtclinicturkiye.com. Details: Name: ${result.name}, email: ${result.email}, phone: ${result.phoneNumber}, message: ${result.message}`,
