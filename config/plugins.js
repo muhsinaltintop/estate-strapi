402bca2ecf2242a4c3d0e71d78faba0ba1571c14
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-06 09:02:46 +0100

    plugin file updated for email configuration

--- config/plugins.js
+++ config/plugins.js
@@ -8,4 +8,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env("DEFAULT_EMAIL"),
+        defaultReplyTo: env("DEFAULT_EMAIL"),
+      },
+    },
+  },
 });
