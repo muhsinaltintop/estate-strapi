18eabcf49716ea9d13501fad21f6978bddad12ec
Muhsin Altıntop <84478795+muhsinaltintop@users.noreply.github.com>
2024-08-06 12:58:16 +0100

    text of form change to html

--- src/api/inqury/content-types/inqury/lifecycles.js
+++ src/api/inqury/content-types/inqury/lifecycles.js
@@ -10,9 +10,15 @@ module.exports = {
             to: "property@theglobalproperty.co.uk",
             from: "noreply@theglobalproperty.co.uk",
             subject: "New Inquiry From ",
-            text: `You have new inqury from healtclinicturkiye.com. Details: Name: ${result.name}, email: ${result.email}, phone: ${result.phoneNumber}, message: ${result.message}`,
-          });
-      } catch (err) {
+            html: `<h1>New Inquiry</h1>
+              <p>You have a new inquiry from <strong>theglobalproperty.co.uk</strong>.</p>
+              <h2>Details:</h2>
+              <p><strong>Name:</strong> ${result.name}</p>
+              <p><strong>Email:</strong> ${result.email}</p>
+              <p><strong>Phone:</strong> ${result.phoneNumber}</p>
+              <p><strong>Message:</strong> ${result.message}</p>,
+            });
+          } catch (err) {
         console.log(err);
       }
     },
